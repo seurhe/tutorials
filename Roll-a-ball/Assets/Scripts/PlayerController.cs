2f4f018e8ff7b2b45e19e94c97c49b5b91d71ca2
seurhe <urban.hedeas@sweco.se>
2013-11-26 13:36:26 +0100

    Init

---
+++ Roll-a-ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		float moveVertical = Input.GetAxis("Vertical");
+		float moveHorizontal = Input.GetAxis("Horizontal");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rigidbody.AddForce(movement * speed * Time.deltaTime);
+	}
+}
