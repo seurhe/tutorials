6578cc07560a9cf94a7b2a7d5433176bbfe41fca
seurhe <urban.hedeas@sweco.se>
2013-11-26 22:16:33 +0100

    Finished the tutorial

--- Roll-a-ball/Assets/Scripts/PlayerController.cs
+++ Roll-a-ball/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,16 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	public float speed;
+	public GUIText countText;
+	public GUIText winText;
+	private int count;
+
+	void Start()
+	{
+		count = 0;
+		SetCountText ();
+		winText.text = "";
+	}
 
 	// Update is called once per frame
 	void FixedUpdate () 
@@ -14,5 +24,24 @@ public class PlayerController : MonoBehaviour {
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
 		rigidbody.AddForce(movement * speed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.tag == "PickUp") 
+		{
+			other.gameObject.SetActive (false);
+			count++;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 11) 
+		{
+			winText.text = "Färdig!!!";
+		}
 	}
 }
