7b9af387b777847c3aab6d6e8141c807a9a09aeb
seurhe <urban.hedeas@sweco.se>
2013-11-28 06:11:42 +0100

    Added Stealth

---
+++ Stealth/Assets/Scripts/AlarmLight.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlarmLight : MonoBehaviour 
+{
+	public float fadeSpeed = 2f;
+	public float highIntensity = 2f;
+	public float lowIntensity = 0.5f;
+	public float changeMargin = 0.2f;
+	public bool alarmOn;
+
+	private float targetIntenity;
+
+	void Awake()
+	{
+		light.intensity = 0f;
+		targetIntenity = highIntensity;
+	}
+
+	void Update()
+	{
+		if (alarmOn) {
+			light.intensity = Mathf.Lerp (light.intensity, targetIntenity, fadeSpeed * Time.deltaTime);
+			CheckTargetIntensity ();
+		} else {
+			light.intensity = Mathf.Lerp(light.intensity, 0f, fadeSpeed * Time.deltaTime);
+		}
+	}
+
+	void CheckTargetIntensity()
+	{
+		if (Mathf.Abs (targetIntenity - light.intensity) < changeMargin) {
+			if (targetIntenity == highIntensity)
+			{
+				targetIntenity = lowIntensity;
+			} else 
+			{
+				targetIntenity = highIntensity;
+			}
+		}
+	}
+}
