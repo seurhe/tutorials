d890b801e7e2234ab5cd43491ab6a7bfcfdb4dd1
seurhe <urban.hedeas@sweco.se>
2014-01-06 10:26:19 +0100

    Stealth tutorial completed

---
+++ Stealth/Assets/Scripts/LevelDynamics/KeyPickup.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyPickup : MonoBehaviour
+{
+	public AudioClip keyGrab;                       // Audioclip to play when the key is picked up.
+	
+	
+	private GameObject player;                      // Reference to the player.
+	private PlayerInventory playerInventory;        // Reference to the player's inventory.
+	
+	
+	void Awake ()
+	{
+		// Setting up the references.
+		player = GameObject.FindGameObjectWithTag(Tags.player);
+		playerInventory = player.GetComponent<PlayerInventory>();
+	}
+	
+	
+	void OnTriggerEnter (Collider other)
+	{
+		// If the colliding gameobject is the player...
+		if(other.gameObject == player)
+		{
+			// ... play the clip at the position of the key...
+			AudioSource.PlayClipAtPoint(keyGrab, transform.position);
+			
+			// ... the player has a key ...
+			playerInventory.hasKey = true;
+			
+			// ... and destroy this gameobject.
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Stealth/Assets/Scripts/Player/PlayerInventory.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInventory : MonoBehaviour
+{
+	public bool hasKey;         // Whether or not the player has the key.
+}
